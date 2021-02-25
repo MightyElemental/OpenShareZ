432a5061a184c6aefc4da5b964086807479e50f7
MightyElemental <MightyElemental@gmail.com>
2021-02-25 15:59:18 +0000

    Reformatted styling

--- opensharez/src/main/java/mightyelemental/opensharez/FullscreenRegionSelectionWindow.java
+++ opensharez/src/main/java/mightyelemental/opensharez/FullscreenRegionSelectionWindow.java
@@ -27,28 +27,27 @@ public class FullscreenRegionSelectionWindow extends JFrame {
 
 	private int oldy, oldx, currentX, currentY;
 
-	private void updateSelection(int newx, int newy) {
+	private void updateSelection( int newx, int newy ) {
 		if (newy - oldy < 0) {
 			if (newx - oldx < 0) {
-				selection = new Rectangle( newx, newy, -newx + oldx, -newy + oldy );
+				selection = new Rectangle(newx, newy, -newx + oldx, -newy + oldy);
 			} else {
-				selection = new Rectangle( oldx, newy, newx - oldx, -newy + oldy );
+				selection = new Rectangle(oldx, newy, newx - oldx, -newy + oldy);
 			}
 		} else {
 			if (newx - oldx < 0) {
-				selection = new Rectangle( newx, oldy, -newx + oldx, newy - oldy );
+				selection = new Rectangle(newx, oldy, -newx + oldx, newy - oldy);
 			} else {
-				selection = new Rectangle( oldx, oldy, newx - oldx, newy - oldy );
+				selection = new Rectangle(oldx, oldy, newx - oldx, newy - oldy);
 			}
 		}
 	}
 
-	public void drawZoom(Graphics g) {
-		int x = Math.max( 0, currentX - 7 );
-		int y = Math.max( 0, currentY - 7 );
-		BufferedImage img = capture.getSubimage( x, y, Math.min( capture.getWidth() - x, 15 ),
-				Math.min( capture.getHeight() - y, 15 ) );
-		g.drawImage( img, currentX + 80, currentY - 80, 150, 150, null );
+	public void drawZoom( Graphics g ) {
+		int x = Math.max(0, currentX - 7);
+		int y = Math.max(0, currentY - 7);
+		BufferedImage img = capture.getSubimage(x, y, Math.min(capture.getWidth() - x, 15), Math.min(capture.getHeight() - y, 15));
+		g.drawImage(img, currentX + 80, currentY - 80, 150, 150, null);
 		// g.drawLine( currentX + 80, currentY + 4, currentX + 80 + 79, currentY + 4 );
 		// g.drawLine( currentX + 80 + 79 + 8, currentY + 4, currentX + 80 + 79 + 80, currentY + 4 );
 
@@ -57,43 +56,43 @@ public class FullscreenRegionSelectionWindow extends JFrame {
 				int gridX = currentX + 80 + ix;
 				int gridY = currentY - 80 + iy;
 				if (ix == 70 && iy == 70) {
-					g.setColor( Color.white );
-					g.drawRect( gridX + 1, gridY + 1, 8, 8 );
-					g.setColor( Color.black );
-					g.drawRect( gridX, gridY, 10, 10 );
+					g.setColor(Color.white);
+					g.drawRect(gridX + 1, gridY + 1, 8, 8);
+					g.setColor(Color.black);
+					g.drawRect(gridX, gridY, 10, 10);
 				} else if (ix == 70 || iy == 70) {
-					g.setColor( new Color( 0.9f, 0.9f, 1f, 0.25f ) );
-					g.fillRect( gridX, gridY, 10, 10 );
+					g.setColor(new Color(0.9f, 0.9f, 1f, 0.25f));
+					g.fillRect(gridX, gridY, 10, 10);
 				}
-				g.setColor( new Color( 0.4f, 0.4f, 0.4f, 0.5f ) );
-				g.drawRect( gridX, gridY, 10, 10 );
+				g.setColor(new Color(0.4f, 0.4f, 0.4f, 0.5f));
+				g.drawRect(gridX, gridY, 10, 10);
 			}
 		}
 
-		g.setColor( Color.black );
-		g.drawRect( currentX + 80, currentY - 80, 150, 150 );
-		g.setColor( Color.white );
-		g.drawRect( currentX + 79, currentY - 81, 152, 152 );
+		g.setColor(Color.black);
+		g.drawRect(currentX + 80, currentY - 80, 150, 150);
+		g.setColor(Color.white);
+		g.drawRect(currentX + 79, currentY - 81, 152, 152);
 
 	}
 
-	public void drawRegion(Graphics g) {
-		g.setColor( new Color( 0.5f, 0.5f, 0.5f, 0.3f ) );
+	public void drawRegion( Graphics g ) {
+		g.setColor(new Color(0.5f, 0.5f, 0.5f, 0.3f));
 		int width = capture.getWidth();
 		int height = capture.getHeight();
 		if (selection == null) {
-			g.fillRect( 0, 0, width, height );
+			g.fillRect(0, 0, width, height);
 		} else {
-			g.fillRect( 0, 0, selection.x, height );
-			g.fillRect( selection.x + selection.width, 0, width - selection.x, height );
+			g.fillRect(0, 0, selection.x, height);
+			g.fillRect(selection.x + selection.width, 0, width - selection.x, height);
 
-			g.fillRect( selection.x, 0, selection.width, selection.y );
-			g.fillRect( selection.x, selection.y + selection.height, selection.width, height - (selection.y + selection.height) );
+			g.fillRect(selection.x, 0, selection.width, selection.y);
+			g.fillRect(selection.x, selection.y + selection.height, selection.width, height - (selection.y + selection.height));
 
 			// g.fillRect( 0, selection.y + selection.height, selection.width, selection.y );
 
-			g.setColor( Color.black );
-			g.drawRect( selection.x, selection.y, selection.width, selection.height );
+			g.setColor(Color.black);
+			g.drawRect(selection.x, selection.y, selection.width, selection.height);
 		}
 
 	}
@@ -101,59 +100,70 @@ public class FullscreenRegionSelectionWindow extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public FullscreenRegionSelectionWindow(BufferedImage img) {
+	public FullscreenRegionSelectionWindow( BufferedImage img ) {
 		this.capture = img;
 		currentX = capture.getWidth() / 2;
 		currentY = capture.getHeight() / 2;
-		setDefaultCloseOperation( DISPOSE_ON_CLOSE );
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		// setBounds( 100, 100, 450, 300 );
 		contentPane = new JPanel() {
 
 			private static final long serialVersionUID = -2060599554374078227L;
 
-			public void paint(Graphics g) {
-				g.drawImage( capture, 0, 0, null );
+			public void paint( Graphics g ) {
+				g.drawImage(capture, 0, 0, null);
 
-				drawRegion( g );
-				drawZoom( g );
+				drawRegion(g);
+				drawZoom(g);
 			}
 
 		};
-		contentPane.addMouseListener( new MouseAdapter() {
+		contentPane.addMouseListener(new MouseAdapter() {
 
 			@Override
-			public void mouseReleased(MouseEvent e) { if (selection != null) { regionSelected = true; System.out.println( "selected" ); } }
+			public void mouseReleased( MouseEvent e ) {
+				if (selection != null) {
+					regionSelected = true;
+					System.out.println("selected");
+				}
+			}
 
 			@Override
-			public void mousePressed(MouseEvent e) {
+			public void mousePressed( MouseEvent e ) {
 				if (e.getButton() == 1) {
 					int x = e.getX();
 					int y = e.getY();
 					oldx = x + 1;
 					oldy = y + 1;
-				} else if (e.getButton() == 3) { cancelled = true; }
+				} else if (e.getButton() == 3) {
+					cancelled = true;
+				}
 			}
-		} );
-		contentPane.addMouseMotionListener( new MouseMotionAdapter() {
+		});
+		contentPane.addMouseMotionListener(new MouseMotionAdapter() {
 
 			@Override
-			public void mouseMoved(MouseEvent e) { currentX = e.getX(); currentY = e.getY(); contentPane.repaint(); }
+			public void mouseMoved( MouseEvent e ) {
+				currentX = e.getX();
+				currentY = e.getY();
+				contentPane.repaint();
+			}
 
 			@Override
-			public void mouseDragged(MouseEvent e) {
+			public void mouseDragged( MouseEvent e ) {
 				currentX = e.getX();
 				currentY = e.getY();
 
 				int x = e.getX();
 				int y = e.getY();
 
-				updateSelection( x, y );
+				updateSelection(x, y);
 				contentPane.repaint();
 			}
-		} );
-		contentPane.setBorder( new EmptyBorder( 5, 5, 5, 5 ) );
-		contentPane.setLayout( new BorderLayout( 0, 0 ) );
-		setContentPane( contentPane );
+		});
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
 	}
 
 }
